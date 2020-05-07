a79aed3813ee2e9e90c6b74bc71c75ac32bc1ba6
chxaitz <345473553@qq.com>
2020-05-07 15:27:02 +0800

    Update efs.h
    1.remove the efs_gc() function.

--- efs/efs.h
+++ efs/efs.h
@@ -26,17 +26,17 @@
 /*********************************************************************
  * CONSTANTS
  */
-#define EFS_KEY_LENGHT_MAX  4   // it is fixed to 4, and do not change it!!!
+#define EFS_KEY_LENGTH_MAX  4   // it is fixed to 4, and no need to change it
 
-#define EFS_POINTER_DEFAULT 0x0000
-#define EFS_POINTER_NULL    0xffff
+#define EFS_POINTER_DEFAULT 0xffff
+#define EFS_POINTER_NULL    0x0000
 #define EFS_START_ADDR      0x0000
 #define EFS_AREA_SIZE       0x0300
 #define EFS_BLOCK_SIZE      0x20
 #define EFS_SECTOR_SIZE     0x80        // the min size can be erase
 #define EFS_INVALID_KEY_MAX 4  //when invalid key to this val, efs will rebuild the MapTable chain to clear the invalid MapTableItem
    
-#define EFS_POINTER_SIZE    sizeof(size_t)
+#define EFS_POINTER_SIZE     sizeof(size_t)
 #define EFS_BLOCK_INDEX_MAX  (EFS_AREA_SIZE/EFS_BLOCK_SIZE)
 #define EFS_SECTOR_INDEX_MAX (EFS_AREA_SIZE/EFS_SECTOR_SIZE)
 #define EFS_BLOCKS_IN_SECTOR (EFS_SECTOR_SIZE/EFS_BLOCK_SIZE)
@@ -57,7 +57,6 @@ uint8_t efs_format();
 size_t  efs_get_len( uint8_t *key );
 uint8_t efs_get( uint8_t *key, uint8_t *buf, size_t bufLen, size_t *dataLen);
 uint8_t efs_set( uint8_t *key, uint8_t *buf, size_t bufLen );
-uint8_t efs_gc();
 
 // the efs port funcs
 uint8_t efs_port_read(size_t addr, uint8_t *buf, size_t size);
